5deb6ae78559b4c83106b2d1b52421a71123bbbb
Bomber <Bomber@Bomber-PC.skynet.lt>
2011-09-18 13:28:46 +0300

    main.cpp - Changed to fractal created at class Fractal image - http://www.mif.vu.lt/~rimask/old/fractals/14.png

--- main.cpp
+++ main.cpp
@@ -10,51 +10,56 @@
 #endif
 
 using namespace std;
+/*
+ * Serpinski_rec_2.c
+ *
+ */
 
-//Called when a key is pressed
-void handleKeypress(unsigned char key, //The key that was pressed
-                                        int x, int y) {    //The current mouse coordinates
-        switch (key) {
-                case 27: //Escape key
-                        exit(0); //Exit the program
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>	// OpenGL Graphics Utility Library
+
+// Number of step
+int CurrentNum = 0;
+const int MaxNum = 10;
+
+// These variables set the dimensions of the rectanglar region we wish to view.
+const double Xmin = -0.1, Xmax = 1.1;
+const double Ymin = -0.1, Ymax = 1.1;
+
+// glutKeyboardFunc is called below to set this function to handle
+//		all "normal" ascii key presses.
+// Only space bar and escape key have an effect.
+void myKeyboardFunc( unsigned char key, int x, int y )
+{
+        switch ( key ) {
+
+        case ' ':									// Space bar
+                // Increment the current step number, and tell operating system screen needs redrawing
+                CurrentNum = (CurrentNum+1)%MaxNum;
+                glutPostRedisplay();
+                break;
+
+        case 27:									// "27" is theEscape key
+                exit(1);
+
         }
 }
 
-//Initializes 3D rendering
-void initRendering() {
-        //Makes 3D drawing work when something is in front of something else
-        glEnable(GL_DEPTH_TEST);
-        glEnable(GL_LIGHTING);
-        glEnable(GL_LIGHT0);
-        glEnable(GL_NORMALIZE);
-        glEnable(GL_COLOR_MATERIAL);
-        glClearColor(0.7f, 0.9f, 1.0f, 1.0f);
-}
-
-//Called when the window is resized
-void handleResize(int w, int h) {
-        //Tell OpenGL how to convert from coordinates to pixel values
-        glViewport(0, 0, w, h);
-
-        glMatrixMode(GL_PROJECTION); //Switch to setting the camera perspective
-
-        //Set the camera perspective
-        glLoadIdentity(); //Reset the camera
-        gluPerspective(45.0,                  //The camera angle
-                                   (double)w / (double)h, //The width-to-height ratio
-                                   1.0,                   //The near z clipping coordinate
-                                   200.0);                //The far z clipping coordinate
-}
-float _angle = 30.0f;
-float _cameraAngle = 0.0f;
 
 void drawFractal(int num)
 {
  switch(num) {
    case 0:
-     glRotatef (_angle, 1,2,0);
-                glBegin( GL_TRIANGLES );
+                glBegin( GL_QUADS );
                 glVertex3f( 1.0, 0.0, 0.0 );
+                glVertex3f( 1.0, 0.2, 0.0 );
+                glVertex3f( 0.0, 0.2, 0.0 );
+                glVertex3f( 0.0, 0.0, 0.0 );
+                glEnd();
+                glBegin( GL_QUADS );
+                glVertex3f( 0.2, 0.0, 0.0 );
+                glVertex3f( 0.2, 1.0, 0.0 );
                 glVertex3f( 0.0, 1.0, 0.0 );
                 glVertex3f( 0.0, 0.0, 0.0 );
                 glEnd();
@@ -62,165 +67,159 @@ void drawFractal(int num)
    default:
         glPushMatrix();
         glPushMatrix();
+        glPushMatrix();
+        //First L
         glScalef(0.5,0.5,1.0);
         drawFractal(num - 1);
         glPopMatrix();
+        //Second L
         glTranslatef(0.5,0.0,0.0);
-        glScalef(0.5,0.5,1.0);
+        glRotatef(-90,0,0,1);
+        glScalef(-0.5,0.5,0.5);
         drawFractal(num - 1);
         glPopMatrix();
-        glTranslatef(0.0,0.5,0.0);
-        glScalef(0.5,0.5,0.5);
+        //Third L
+        glTranslatef(0.5,0.5,0.0);
+        glScalef(-0.5,0.5,0.5);
+        drawFractal(num - 1);
+        //Fourth L
+        glPopMatrix ();
+        glTranslatef(1.0,1.0,0.0);
+        glRotatef(-180,0,0,1);
+        glScalef(0.25,0.25,0.25);
         drawFractal(num - 1);
         break;
  }
 }
-//Draws the 3D scene
-void drawScene() {
-        //Clear information from last draw
+
+/*
+ * drawScene() handles the animation and the redrawing of the
+ *		graphics window contents.
+ */
+void drawScene(void)
+{
+        // Clear the rendering window
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor (1.0,1.0,1.0,0.0);
 
-        glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
-        glLoadIdentity(); //Reset the drawing perspective
-        glRotatef (_cameraAngle,0,1,0);
-        glTranslatef (0.0f, 0.0f, -5.0f);//Move forward 5units
+        // Set drawing color to white
+        glColor3f(0.0, 0.0, 0.0 );
 
-        //Add ambient light
-        GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color(0.2, 0.2, 0.2)
-        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+    glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+    drawFractal(CurrentNum);
 
-        //Add positioned light
-        GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
-        GLfloat lightPos0[] = {4.0f, 0.0f, 1.25f, 1.0f}; //Positioned at (4, 0, 8)
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
-        glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
-
-        glPushMatrix ();//Save transformations
-        glTranslatef (0.0f, -1.0f, 0);
-        glRotatef (_angle,1,0,0);
-        glBegin(GL_QUADS); //Begin quadrilateral coordinates
-
-        //Trapezoid
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex3f(-0.7f, -0.5f, 0.0f);
-        glVertex3f(0.7f, -0.5f, 0.0f);
-        glColor3f(1.0f, 0.75f, 0.0f);
-        glVertex3f(0.4f, 0.5f, 0.0f);
-        glVertex3f(-0.4f, 0.5f, 0.0f);
-
-        glVertex3f(0.4f, 0.5f, 0.0f);
-        glVertex3f(-0.4f, 0.5f, 0.0f);
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex3f(-0.7f, -0.5f, 0.5f);
-        glVertex3f(0.7f, -0.5f, 0.5f);
-
-        glEnd(); //End quadrilateral coordinates
-
-        glPopMatrix(); //Undo the move to the center of the trapezoid
-        glPushMatrix(); //Save the current state of transformations
-        glTranslatef(1.0f, 1.0f, 0.0f); //Move to the center of the pentagon
-        glRotatef (_angle, 0,1,0);
-        glScalef (0.7,0.7,0.7);
-
-        glBegin(GL_TRIANGLES); //Begin triangle coordinates
-
-        //Pentagon frontside
-        glNormal3f(0.0f, 0.0f, -0.5f);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-
-        glNormal3f(0.0f, 0.0f, -0.5f);
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glVertex3f(0.5f, 0.0f, 0.0f);
-
-        glNormal3f(0.0f, 0.0f, -0.5f);
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-        glVertex3f(0.5f, 0.0f, 0.0f);
-        glVertex3f(0.0f, 0.5f, 0.0f);
-
-        //Pentagon backside
-        glNormal3f(0.0f, 0.0f, 0.5f);
-        glVertex3f(-0.5f, -0.5f, 0.5f);
-        glVertex3f(0.5f, -0.5f, 0.5f);
-        glVertex3f(-0.5f, 0.0f, 0.5f);
-
-        glNormal3f(0.0f, 0.0f, 0.5f);
-        glVertex3f(-0.5f, 0.0f, 0.5f);
-        glVertex3f(0.5f, -0.5f, 0.5f);
-        glVertex3f(0.5f, 0.0f, 0.5f);
-
-        glNormal3f(0.0f, 0.0f, 0.5f);
-        glVertex3f(-0.5f, 0.0f, 0.5f);
-        glVertex3f(0.5f, 0.0f, 0.5f);
-        glVertex3f(0.0f, 0.5f, 0.5f);
-
-        glEnd();
-        glBegin(GL_QUADS); //Begin quadrilateral coordinates
-        //Pentagon rightside
-        glNormal3f(0.5f, 0.0f, 0.0f);
-        glVertex3f(0.5f, 0.0f, 0.5f);
-        glVertex3f(0.5f, -0.5f, 0.5f);
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glVertex3f(0.5f, 0.0f, 0.0f);
-
-        //Pentagon leftside
-        glNormal3f(-0.5f, 0.0f, 0.0f);
-        glVertex3f(-0.5f, 0.0f, 0.5f);
-        glVertex3f(-0.5f, -0.5f, 0.5f);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-        glVertex3f(-0.5f, 0.0f, 0.0f);
-
-        glEnd();
-
-
-        glPopMatrix(); //Undo the move to the center of the pentagon
-        glPushMatrix(); //Save the current state of transformations
-        glTranslatef(-1.0f, 1.0f, 0.0f); //Move to the center of the triangle
-        glRotatef (_angle,1,0,0);
-
-        glBegin (GL_TRIANGLES);
-
-        //Triangle
-        glVertex3f(0.5f, -0.5f, 0.0f);
-        glVertex3f(0.0f, 0.5f, 0.0f);
-        glVertex3f(-0.5f, -0.5f, 0.0f);
-
-        glEnd(); //End triangle coordinates
-        glPopMatrix ();
-
-        int num = 5;
-        drawFractal(num);
-
-        glutSwapBuffers(); //Send the 3D scene to the screen
+ // Flush the pipeline.  (Not usually necessary.)
+        glFlush();
 }
-void update(int value) {
-    _angle += 2.0f;
-    if (_angle > 360) {
-        _angle -= 360;
-    }
 
-    glutPostRedisplay(); //Tell GLUT that the scene has changed
+// Initialize OpenGL's rendering modes
+void initRendering()
+{
 
-    //Tell GLUT to call update again in 25 milliseconds
-    glutTimerFunc(25, update, 0);
+        glEnable ( GL_DEPTH_TEST );
+        glClearColor (1.0,1.0,1.0,0.0);
+
+        // Uncomment out the first block of code below, and then the second block,
+        //		to see how they affect line and point drawing.
+/*
+        // The following commands should cause points and line to be drawn larger
+        //	than a single pixel width.
+        glPointSize(8);
+        glLineWidth(5);
+*/
+
+/*
+        // The following commands should induce OpenGL to create round points and
+        //	antialias points and lines.  (This is implementation dependent unfortunately).
+        glEnable(GL_POINT_SMOOTH);
+        glEnable(GL_LINE_SMOOTH);
+        glHint(GL_POINT_SMOOTH_HINT, GL_NICEST);	// Make round points, not square points
+        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);		// Antialias the lines
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+*/
+
 }
-int main(int argc, char** argv) {
-    //Initialize GLUT
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(400, 400); //Set the window size
 
-    //Create the window
-    glutCreateWindow("Basic Shapes - videotutorialsrock.com");
-    initRendering(); //Initialize rendering
+// Called when the window is resized
+//		w, h - width and height of the window in pixels.
+void resizeWindow(int w, int h)
+{
+        double scale, center;
+        double windowXmin, windowXmax, windowYmin, windowYmax;
 
-    //Set handler functions for drawing, keypresses, and window resizes
-    glutDisplayFunc(drawScene);
-    glutKeyboardFunc(handleKeypress);
-    glutReshapeFunc(handleResize);
-    glutTimerFunc(25, update, 0); //Add a timer
-    glutMainLoop(); //Start the main loop.  glutMainLoop doesn't return.
-    return 0; //This line is never reached
+        // Define the portion of the window used for OpenGL rendering.
+        glViewport( 0, 0, w, h );	// View port uses whole window
+
+        // Set up the projection view matrix: orthographic projection
+        // Determine the min and max values for x and y that should appear in the window.
+        // The complication is that the aspect ratio of the window may not match the
+        //		aspect ratio of the scene we want to view.
+        w = (w==0) ? 1 : w;
+        h = (h==0) ? 1 : h;
+        if ( (Xmax-Xmin)/w < (Ymax-Ymin)/h ) {
+                scale = ((Ymax-Ymin)/h)/((Xmax-Xmin)/w);
+                center = (Xmax+Xmin)/2;
+                windowXmin = center - (center-Xmin)*scale;
+                windowXmax = center + (Xmax-center)*scale;
+                windowYmin = Ymin;
+                windowYmax = Ymax;
+        }
+        else {
+                scale = ((Xmax-Xmin)/w)/((Ymax-Ymin)/h);
+                center = (Ymax+Ymin)/2;
+                windowYmin = center - (center-Ymin)*scale;
+                windowYmax = center + (Ymax-center)*scale;
+                windowXmin = Xmin;
+                windowXmax = Xmax;
+        }
+
+        // Now that we know the max & min values for x & y that should be visible in the window,
+        //		we set up the orthographic projection.
+        glMatrixMode( GL_PROJECTION );
+        glLoadIdentity();
+        glOrtho( windowXmin, windowXmax, windowYmin, windowYmax, -1, 1 );
+
 }
+
+
+// Main routine
+// Set up OpenGL, define the callbacks and start the main loop
+int main( int argc, char** argv )
+{
+        glutInit(&argc,argv);
+
+        // The image is not animated so single buffering is OK.
+        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH );
+
+        // Window position (from top corner), and size (width and hieght)
+        glutInitWindowPosition( 20, 60 );
+        glutInitWindowSize( 500, 500 );
+        glutCreateWindow( "SimpleDraw - Press space bar to toggle images" );
+
+        // Initialize OpenGL as we like it..
+        initRendering();
+
+        // Set up callback functions for key presses
+        glutKeyboardFunc( myKeyboardFunc );			// Handles "normal" ascii symbols
+        // glutSpecialFunc( mySpecialKeyFunc );		// Handles "special" keyboard keys
+
+        // Set up the callback function for resizing windows
+        glutReshapeFunc( resizeWindow );
+
+        // Call this for background processing
+        // glutIdleFunc( myIdleFunction );
+
+        // call this whenever window needs redrawing
+        glutDisplayFunc( drawScene );
+
+        fprintf(stdout, "Press space bar for the next step; escape button to quit.\n");
+
+        // Start the main loop.  glutMainLoop never returns.
+        glutMainLoop(  );
+
+        return(0);	// This line is never reached.
+}
+
